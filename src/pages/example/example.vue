bb02a272e87961486bd8103a7b8cd704e3e20fa4
kerakawachi <kerakawachi.tsutomu@jp.panasonic.com>
2024-09-12 19:34:30 +0900

    わざと改変

--- src/pages/example/example.vue
+++ src/pages/example/example.vue
@@ -8,7 +8,30 @@ import example from '../../components/example.vue'
 export default {
   name: 'App',
   components: {
-    'example': example
+    'example': example,
+    'example2': example,
+    'example3': example,
+    'example4': example
+  },
+  // 変数定義
+  data() {
+    return {
+    }
+  },
+  // カスタムディレクティブをローカルに登録
+  // directives: {
+  // },
+  // インスタンス生成時に実行したい処理を記述
+  created() {
+  },
+  // 算出プロパティ。動的に変化する値のgetterなど記述
+  computed: {
+  },
+  // 監視プロパティ。ほかの値が変化したときに計算しなおしたい値がある場合に記述
+  watch: {
+  },
+  // ロジック部
+  methods: {
   }
 }
 </script>
